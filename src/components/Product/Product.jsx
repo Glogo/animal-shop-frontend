58459a178e57789214eb4df935aed8ec6689f522
Michael Gloger <gloger.michael@gmail.com>
2019-06-11 18:23:07 +0200

    Basic product list layout

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import './Product.css';
 
 export default function Product({
     id, name, price, thumbnail,
 }) {
     return (
-        <div className="column is-one-quarter">
-            <div className="product card">
+        <div className="product column is-one-quarter">
+            <div className="card">
                 <div className="card-image">
-                    <figure className="image is-4by3">
+                    <figure className="image is-1by1">
                         <img src={`/api/images/${thumbnail}`} alt={name} />
                     </figure>
                 </div>
                 <div className="card-content">
-                    <h3 className="is-size-5">{name}</h3>
+                    <h3 className="is-size-5">
+                        <Link to={`/products/${id}`}>{name}</Link>
+                    </h3>
+                </div>
+                <div className="product-price is-size-5">
                     {price} €
                 </div>
             </div>
